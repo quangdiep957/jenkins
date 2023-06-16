c49e79747e1924800795bbad8775f9b569a65290
quangdiep957 <95072098+quangdiep957@users.noreply.github.com>
2023-06-16 17:26:53 +0700

    Update jobpulish.groovy

--- vars/jobpulish.groovy
+++ vars/jobpulish.groovy
@@ -1,7 +1,7 @@
 def call() {
   // Khai báo các hàm dùng chung
   STRING_DELIMITER = ','
-  WORKSPACE = '.'
+  WORKSPACE = 'sourcecode'
   URL_FRONTEND = 'https://github.com/quangdiep957/BookingRoomVue.git'
   URL_BACKEND = 'https://github.com/quangdiep957/BookingRoomAPI.git'
 
