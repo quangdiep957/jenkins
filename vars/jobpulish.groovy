ca6fd9a3976208b7cd074bcb1c5f40287658d01a
quangdiep957 <95072098+quangdiep957@users.noreply.github.com>
2023-06-16 16:03:33 +0700

    Update jobpulish.groovy

--- vars/jobpulish.groovy
+++ vars/jobpulish.groovy
@@ -10,48 +10,49 @@ def call() {
       label 'linux'
     }
 
-   parameters{
-            extendedChoice( 
-                name: 'APP', 
-                value: 'frontend,backend', 
-                multiSelectDelimiter: STRING_DELIMITER, 
-                quoteValue: false,
-                saveJSONParameterToFile: false,
-                type: 'PT_CHECKBOX', 
-                description: 'Chon app build?', 
-            )
-            string(
-                defaultValue: 'master',
-                name: 'FRONTEND_GIT_BRANCH',
-                trim: true,
-                description: 'Build Front End nhánh nào?',
-            )
-            string(
-                defaultValue: 'master',
-                name: 'BACKEND_GIT_BRANCH',
-                trim: true,
-                description: 'Build Back End nhánh nào?',
-            )
-        }
+    parameters {
+      extendedChoice( 
+        name: 'APP', 
+        value: 'frontend,backend', 
+        multiSelectDelimiter: STRING_DELIMITER, 
+        quoteValue: false,
+        saveJSONParameterToFile: false,
+        type: 'PT_CHECKBOX', 
+        description: 'Chọn app build?', 
+      )
+      string(
+        defaultValue: 'master',
+        name: 'FRONTEND_GIT_BRANCH',
+        trim: true,
+        description: 'Build Front End nhánh nào?',
+      )
+      string(
+        defaultValue: 'master',
+        name: 'BACKEND_GIT_BRANCH',
+        trim: true,
+        description: 'Build Back End nhánh nào?',
+      )
+    }
 
     stages {
-      // Get source code mới nhất
+      // Get latest source code
       stage('Get latest source code') {
         steps {
           script {
             def sourceTasks = [:]
             // Lấy ra các app cần build
-            def appBuild = ['backend']
+            def appBuild = params.APP ?: ['backend']
             echo appBuild
+
             // Xây dựng các tác vụ checkout cho từng app
             appBuild.each { app ->
               sourceTasks[app] = {
                 dir(WORKSPACE) {
                   script {
                     if (app == 'frontend') {
-                      checkout([$class: 'GitSCM', branches: [[name: 'main']], userRemoteConfigs: [[url: URL_FRONTEND]]])
+                      checkout([$class: 'GitSCM', branches: [[name: params.FRONTEND_GIT_BRANCH ?: 'main']], userRemoteConfigs: [[url: URL_FRONTEND]]])
                     } else if (app == 'backend') {
-                      checkout([$class: 'GitSCM', branches: [[name: 'main']], userRemoteConfigs: [[url: URL_BACKEND]]])
+                      checkout([$class: 'GitSCM', branches: [[name: params.BACKEND_GIT_BRANCH ?: 'main']], userRemoteConfigs: [[url: URL_BACKEND]]])
                     }
                   }
                 }
