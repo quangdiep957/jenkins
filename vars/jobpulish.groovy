1467552c66470810ebdbd878cd54725da76793ce
quangdiep957 <95072098+quangdiep957@users.noreply.github.com>
2023-06-16 16:28:14 +0700

    Update jobpulish.groovy

--- vars/jobpulish.groovy
+++ vars/jobpulish.groovy
@@ -21,13 +21,13 @@ def call() {
         description: 'Chọn app build?', 
       )
       string(
-        defaultValue: 'master',
+        defaultValue: 'main',
         name: 'FRONTEND_GIT_BRANCH',
         trim: true,
         description: 'Build Front End nhánh nào?',
       )
       string(
-        defaultValue: 'master',
+        defaultValue: 'main',
         name: 'BACKEND_GIT_BRANCH',
         trim: true,
         description: 'Build Back End nhánh nào?',
@@ -59,6 +59,43 @@ def call() {
 
             // Thực hiện các tác vụ checkout song song
             parallel sourceTasks
+          }
+        }
+        stage('publish các app')
+        {
+          steps
+          {
+            script
+            {
+              def sourceTasks = [:]
+                // Lấy ra các app cần build
+                def appBuild = params.APP ? params.APP.split(STRING_DELIMITER): []
+                // publish từng app
+                appBuild.each { app ->
+                  sourceTasks[app] = {
+                    dir(WORKSPACE) {
+                      script {
+                        if (app == 'frontend') {
+                          stage('npm build')
+                          {
+                             def commands = [
+                                   ''node -v',
+                                   'npm i -g @vue/cli',
+                                   // 'npm i',
+                                   'npm run build'
+                                   ]
+                                   commands.each{i ->
+                                   runCmd(i)
+                                  }
+                          }
+                        } else if (app == 'backend') {
+                          checkout([$class: 'GitSCM', branches: [[name: params.BACKEND_GIT_BRANCH ?: 'main']], userRemoteConfigs: [[url: URL_BACKEND]]])
+                        }
+                      }
+                    }
+                  }
+                }
+            }
           }
         }
       }
